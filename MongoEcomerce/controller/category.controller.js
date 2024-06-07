8c1e5002925de2f6a838c2e8424f8c0e76a8f2f9
Vinod <sagar.choudhary9811@gmail.com>
2024-06-07 11:53:18 -0500

    Updated

--- MongoEcomerce/controller/category.controller.js
+++ MongoEcomerce/controller/category.controller.js
@@ -1,14 +1,7 @@
 import category from "../model/category.model";
 
-<<<<<<< HEAD
 export const newCategory = async (req, res) => {
     const newCa = await category.create({
         categoryName: req.body
     })
 }
-=======
-export const newCategory = async (req,res)=>{
-    const newCa= await category.create({
-        categoryName: req.body
-})}
->>>>>>> 0a379cd49ea7260f16d85db1196c700928396ff5
